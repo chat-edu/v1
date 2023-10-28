ae8e345e1a63298f6d88ddeeb26b1ba272f801b6
jphiggzz <john.p.higgins@vanderbilt.edu>
2023-10-28 15:18:57 -0500

    add firestore collections, documents, and converters

---
+++ firebase/firestore/converters/notesConverter.ts
@@ -0,0 +1,42 @@
+import {
+    DocumentData,
+    FirestoreDataConverter,
+    QueryDocumentSnapshot,
+    SnapshotOptions,
+    WithFieldValue
+} from "@firebase/firestore";
+
+import { notesCollection as baseNotesCollection } from "@/firebase/firestore/collections";
+
+import {Note} from "@/types/Note";
+
+
+// converts a course document to a Course object, allowing for typed queries and strict type checking
+const noteConverter: FirestoreDataConverter<Note> = {
+    toFirestore(note: WithFieldValue<Note>): DocumentData {
+        return {
+            id: note.id,
+            title: note.title,
+            content: note.content,
+            courseId: note.courseId,
+        };
+    },
+    fromFirestore(
+        snapshot: QueryDocumentSnapshot,
+        options: SnapshotOptions
+    ): Note {
+        const data = snapshot.data(options);
+        return {
+            id: snapshot.id,
+            title: data.title,
+            content: data.content,
+            courseId: data.courseId,
+        };
+    },
+};
+
+// collection reference for querying courses
+const notesCollection = (userId: string, courseId: string) =>
+    baseNotesCollection(userId, courseId).withConverter(noteConverter);
+
+export default notesCollection;
