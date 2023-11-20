8e01f2217580e8c6f7a72beeb37e95f587e94115
jason <jason@hedmans.org>
2023-11-20 17:39:21 -0500

    add user event emitter

--- hooks/queries/useUser.ts
+++ hooks/queries/useUser.ts
@@ -1,14 +1,31 @@
-import useAuth from "@/hooks/auth/useAuth";
+import {useCallback, useEffect} from "react";
 
+import useAuth from "@/hooks/auth/useAuth";
 import useItemData from "@/hooks/queries/useItemData";
 
+import {subscribeToUsersChangedEvent, unsubscribeFromUsersChangedEvent} from "@/eventEmitters/userEventEmitter";
+
 import {User} from "@/types/User";
+
 
 const useUser = () => {
 
     const { user } = useAuth();
 
     const [userData, loading, error, fetchUserData] = useItemData<User>(`/api/users/${user?.uid}`);
+
+    const handleUserChanged = useCallback(async (changedUserId: string) => {
+        if(changedUserId === user?.uid) {
+            await fetchUserData();
+        }
+    }, [fetchUserData, user?.uid])
+
+    useEffect(() => {
+        subscribeToUsersChangedEvent(handleUserChanged);
+        return () => {
+            unsubscribeFromUsersChangedEvent(handleUserChanged)
+        };
+    }, [handleUserChanged]);
 
     return {
         user,
