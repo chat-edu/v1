b9a65d8c7ab4acb569fce39623d694696d3e1e30
Jason Hedman <40368124+jasonhedman@users.noreply.github.com>
2023-11-20 23:48:05 -0500

    Update client.ts

--- cosmos/client.ts
+++ cosmos/client.ts
@@ -1,5 +1,7 @@
 import {CosmosClient} from "@azure/cosmos";
 
+console.log(process.env.COSMOS_ENDPOINT)
+
 const COSMOS_CLIENT: CosmosClient = new CosmosClient({
     endpoint: process.env.COSMOS_ENDPOINT as string,
     key: process.env.COSMOS_KEY as string,
