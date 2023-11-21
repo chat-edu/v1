bf2524a53a89dc2ae78ab11fe9faa73d81397ae2
Jason Hedman <40368124+jasonhedman@users.noreply.github.com>
2023-11-21 13:25:00 -0500

    Add logging of environment

--- app/api/notebooks/[notebookId]/route.ts
+++ app/api/notebooks/[notebookId]/route.ts
@@ -3,7 +3,8 @@ import {getNotebook} from "@/cosmos/services/notebooks";
 import {NotebookIdParams} from "@/app/api/notebooks/[notebookId]/NotebookIdParams";
 
 export const GET = async (request: Request, {params}: {params: NotebookIdParams}) => {
-    console.log(request);
     console.log(params.notebookId);
+    console.log(process.env.COSMOS_ENDPOINT);
+    console.log(process.env);
     return Response.json(await getNotebook(params.notebookId));
 }
