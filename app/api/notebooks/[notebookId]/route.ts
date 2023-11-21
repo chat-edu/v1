929971e033edb72395787b3bfaab6708758ff67e
Jason Hedman <40368124+jasonhedman@users.noreply.github.com>
2023-11-21 13:16:37 -0500

    Add logging to dynamic route

--- app/api/notebooks/[notebookId]/route.ts
+++ app/api/notebooks/[notebookId]/route.ts
@@ -3,5 +3,7 @@ import {getNotebook} from "@/cosmos/services/notebooks";
 import {NotebookIdParams} from "@/app/api/notebooks/[notebookId]/NotebookIdParams";
 
 export const GET = async (request: Request, {params}: {params: NotebookIdParams}) => {
+    console.log(request);
+    console.log(params.notebookId);
     return Response.json(await getNotebook(params.notebookId));
 }
