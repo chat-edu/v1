204e544b5e6a296688d2d70a59690d841c3287c7
jason <jason@hedmans.org>
2023-11-22 10:08:12 -0500

    add Apple touch icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,8 @@ const HomePage: NextPage = () => {
               <meta name="description" content="Supercharge your learning with AI" />
               <meta name="viewport" content="width=device-width, initial-scale=1" />
               <link rel="icon" href="/favicon.ico" />
+              <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
+              <link rel="apple-touch-icon" sizes="192x192" href="/apple-touch-icon-192x192.png" />
           </Head>
           <Layout
             authGate
