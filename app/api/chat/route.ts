201d640005ec54a2c81c037e241602cad288c36f
Jason Hedman <40368124+jasonhedman@users.noreply.github.com>
2023-11-22 14:35:41 -0500

    Update CORS on chat endpoint

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -24,5 +24,13 @@ export async function POST(req: Request) {
 
   // @ts-ignore
   const stream = OpenAIStream(response);
-  return new StreamingTextResponse(stream);
+  return new StreamingTextResponse(
+      stream, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        }
+      }
+  );
 }
