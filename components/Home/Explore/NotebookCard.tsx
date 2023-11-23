98e62afd9d34d09680580ecf38b1606846ac0b1b
jason <jason@hedmans.org>
2023-11-23 11:11:29 -0500

    update user card display

--- components/Home/Explore/NotebookCard.tsx
+++ components/Home/Explore/NotebookCard.tsx
@@ -4,13 +4,25 @@ import Link from "next/link";
 
 import ClickableCard from "@/components/Utilities/ClickableCard";
 
+import {Badge, HStack, Progress, Text, VStack} from "@chakra-ui/react";
+
 import {Notebook} from "@/types/Notebook";
 
 interface Props {
     notebook: Notebook
 }
 
+const tags = [
+    'Python',
+    'Machine Learning',
+]
+
 const NotebookCard: React.FC<Props> = ({ notebook }) => {
+
+    const [progress] = React.useState(Math.floor(Math.random() * 100));
+
+    const [score] = React.useState(Math.floor(Math.random() * 8));
+
     return (
         <Link
             href={`/notebook/${notebook.id}`}
@@ -23,7 +35,70 @@ const NotebookCard: React.FC<Props> = ({ notebook }) => {
                 flex={1}
                 w={'100%'}
             >
-                {notebook.name}
+                <VStack
+                    align={'start'}
+                    w={'100%'}
+                >
+                    <HStack
+                        w={'100%'}
+                        align={'start'}
+                    >
+                        <VStack
+                            flex={1}
+                            align={'start'}
+                        >
+                            <HStack>
+                                {
+                                    tags.map(tag => (
+                                        <Badge
+                                            key={tag}
+                                            colorScheme={'brand'}
+                                            cursor={'pointer'}
+                                        >
+                                            {tag}
+                                        </Badge>
+                                    ))
+                                }
+                            </HStack>
+                            <Text
+                                fontWeight={'bold'}
+                                fontSize={{
+                                    base: 'sm',
+                                    md: 'md'
+                                }}
+                                mb={0}
+                            >
+                                {notebook.name}
+                            </Text>
+                            <Text
+                                fontSize={{
+                                    base: 'sm',
+                                    // md: 'md'
+                                }}
+                                opacity={0.7}
+                            >
+                                By Mink ZaZa
+                            </Text>
+                        </VStack>
+                        <Text>
+                            #{score}
+                        </Text>
+                    </HStack>
+                    <HStack
+                        w={'100%'}
+                    >
+                        <Progress
+                            value={progress}
+                            w={'100%'}
+                            colorScheme={'brand'}
+                        />
+                        <Text
+                            fontSize={'xs'}
+                        >
+                            {progress}%
+                        </Text>
+                    </HStack>
+                </VStack>
             </ClickableCard>
         </Link>
     );
