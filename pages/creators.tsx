9088cbd2a96c8787544e7966f775e9e72608d5fa
jason <jason@hedmans.org>
2023-11-29 19:16:31 -0600

    update OG image links

--- pages/creators.tsx
+++ pages/creators.tsx
@@ -14,6 +14,7 @@ const CreatorsPage: NextPage = () => {
                 <title>Creators</title>
                 <meta name="description" content="Supercharge your learning with AI" />
                 <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1.0, user-scalable=no, user-scalable=0;" />
+                <meta property="og:image" content="https://www.chatedu.tech/api/og/creators"/>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
                 <link rel="apple-touch-icon" sizes="192x192" href="/apple-touch-icon-192x192.png" />
--- pages/notebooks/[notebookId].tsx
+++ pages/notebooks/[notebookId].tsx
@@ -14,7 +14,7 @@ const NotebookPage = ({ notebookId } : {notebookId: string}) => {
                 <title>Notebook</title>
                 <meta name="description" content="Supercharge your learning with AI" />
                 <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1.0, user-scalable=no, user-scalable=0;" />
-                <meta property="og:image" content={`https://preview.chatedu.io/api/og/notebooks/${notebookId}`}/>
+                <meta property="og:image" content={`https://www.chatedu.tech/api/og/notebooks/${notebookId}`}/>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
                 <link rel="apple-touch-icon" sizes="192x192" href="/apple-touch-icon-192x192.png" />
