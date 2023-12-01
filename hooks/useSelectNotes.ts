29ec0a517c1935439f7a483dd116aa3fe494c8b7
jason <jason@hedmans.org>
2023-11-30 21:50:41 -0600

    update some no user components

--- hooks/useSelectNotes.ts
+++ hooks/useSelectNotes.ts
@@ -2,15 +2,18 @@ import {useEffect, useState} from "react";
 
 import {Note} from "@/types/Note";
 import useNote from "@/hooks/queries/notes/useNote";
+import useAuth from "@/hooks/useAuth";
 
 const useSelectNotes = (initialNoteId?: Note["id"]) => {
+
+    const { user } = useAuth();
 
     const [selectedNotes, setSelectedNotes] = useState<Note[]>([]);
 
     const { note } = useNote(initialNoteId || 0);
 
     useEffect(() => {
-        if (note && !selectedNotes.find((selectedNote) => selectedNote.id === note.id)) {
+        if (note && !selectedNotes.find((selectedNote) => selectedNote.id === note.id) && user) {
             addNote(note);
         }
     }, [note]);
