eae60f4aa4fc1757dc8e95df0e0fd6096857f472
jason <jason@hedmans.org>
2024-02-04 14:09:36 -0600

    patch teacher notebooks API route

--- app/api/notebooks/teacher/[teacherId]/route.ts
+++ app/api/notebooks/teacher/[teacherId]/route.ts
@@ -2,6 +2,6 @@ import {findNotebooksByTeacherId} from "@/cosmosPostgres/services/notebooks";
 
 import {TeacherIdParams} from "@/app/api/notebooks/teacher/[teacherId]/TeacherIdParams";
 
-export const GET = (req: Request, { params }: { params: TeacherIdParams }) => {
-    return Response.json(findNotebooksByTeacherId(params.teacherId));
+export const GET = async (req: Request, { params }: { params: TeacherIdParams }) => {
+    return Response.json(await findNotebooksByTeacherId(params.teacherId));
 }
