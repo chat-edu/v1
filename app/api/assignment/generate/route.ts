658c7c080c18bcd347f0b08fffbf6ace65bcab3a
murrlincoln <birdbamboo22@gmail.com>
2024-02-21 16:14:07 -0600

    GenerateAssignmentPrompt migrated

--- app/api/assignment/generate/route.ts
+++ app/api/assignment/generate/route.ts
@@ -8,6 +8,7 @@ import {
     findFreeResponseQuestionsByAssignmentId,
     findMultipleChoiceQuestionsByAssignmentId
 } from "@/cosmosPostgres/services/questions";
+import generateAssignmentPrompt from "@/prompts/commands/generateAssignmentPrompt";
 
 export const POST = async (req: Request) => {
     const body = await req.json();
@@ -35,64 +36,14 @@ export const POST = async (req: Request) => {
     const questions = [...multipleChoiceQuestions, ...freeResponseQuestions]
         .sort((a, b) => a.question_number - b.question_number);
 
+    const prompt = generateAssignmentPrompt(notes, questions);
+
     const response = await openai.chat.completions.create({
         model: process.env.GPT_MODEL_ID as string,
         messages: [
             {
                 role: "system",
-                content: `
-                    Given an array of notes, generate an assignment that includes 5 questions. These questions can be either multiple choice or free response. 
-    
-                    Free response questions should be either understanding questions, which challenge students to demonstrate their understanding of a particular concept, or application questions, which challenge students to apply their understanding to a particular problem. 
-    
-                    The questions should flow logically, where completion of each question leads to the sentiment of the next question. 
-    
-                    The questions must be gradeable by AI later in the pipeline.
-                    
-                    Multiple choice questions must take the following JSON format:
-                    
-                   {
-                        tag: "${QuestionTypes.MultipleChoice}",
-                        question: ${JSON.stringify(multipleChoiceCommand.responseFormatting)}
-                    }
-                    
-                    Understanding questions must take the following JSON format:
-                    
-                    {
-                        tag: "${QuestionTypes.FreeResponse}",
-                        question: ${JSON.stringify(understandingQuestionCommand.responseFormatting)}
-                    }
-                    
-                    Understanding questions must take the following JSON format:
-                    
-                    {
-                        tag: "${QuestionTypes.FreeResponse}",
-                        question: ${JSON.stringify(applicationQuestionCommand.responseFormatting)}
-                    }
-                    
-                    Notes:
-                    
-                    ${notes.map((note) => `
-                        ${note.content}
-                    `)}
-                    
-                    ${
-                        questions.length > 0 ? `
-                            The following questions have already been generated:
-                            
-                            ${questions.map((question) => `
-                                ${question.question}
-                            `)}
-                        ` : ""
-                    }
-                    
-                    The whole response should take the following format:
-                    
-                   {
-                        name: <Assignment Name>,
-                        questions: [<Question 1>, <Question 2>, ...]
-                    }                 
-                `,
+                content: prompt,
             }
         ],
         response_format: {
