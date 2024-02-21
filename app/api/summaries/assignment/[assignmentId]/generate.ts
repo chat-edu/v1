1e4e8b471e6d78e0e34f822fb7c805d9f5b2dbea
murrlincoln <birdbamboo22@gmail.com>
2024-02-21 16:34:56 -0600

    Class summary prompt migrated

--- app/api/summaries/assignment/[assignmentId]/generate.ts
+++ app/api/summaries/assignment/[assignmentId]/generate.ts
@@ -1,35 +1,20 @@
 import {findUserAssignmentSummariesByAssignmentId} from "@/cosmosPostgres/services/summaries";
 import openai from "@/openai";
+import generateClassSummaryPrompt from "@/prompts/commands/generateClassSummaryPrompt";
 
 import {Assignment} from "@/types/assignment/Assignment";
 
 export const generateAssignmentSummary = async (assignmentId: Assignment["id"]): Promise<string | null> => {
     const userAssignmentSummaries = await findUserAssignmentSummariesByAssignmentId(assignmentId);
 
+    const prompt = generateClassSummaryPrompt(userAssignmentSummaries);
+
     const response = await openai.chat.completions.create({
         model: process.env.GPT_MODEL_ID as string,
         messages: [
             {
                 role: "system",
-                content: `
-                    Each student's performance on the assignment is summarized in two sentences.
-
-                    Your goal is to provide a two sentence summary of the class's performance and understanding of the course material, which will inform the teacher of their students' strengths and knowledge gaps.
-                    
-                    The summaries are as follows:
-
-                    ${userAssignmentSummaries.map((summary) => JSON.stringify(summary)).join("\n")}
-
-                    Respond in the second person as if you are talking to the teacher.
-                    
-                    Respond in JSON format with the following structure:
-
-                    {
-                        summary: <string>
-                    }
-                    
-                    The summary should use markdown to format the text, bolding the most important information with asterisks.
-                `,
+                content: prompt,
             }
         ],
         response_format: {
