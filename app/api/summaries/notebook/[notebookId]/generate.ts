2d19dbb11ac85affaa2e205c17a965dbb7a06796
murrlincoln <birdbamboo22@gmail.com>
2024-02-21 16:53:23 -0600

    Notebook summary prompt migrated

--- app/api/summaries/notebook/[notebookId]/generate.ts
+++ app/api/summaries/notebook/[notebookId]/generate.ts
@@ -5,34 +5,19 @@ import {
 } from "@/cosmosPostgres/services/summaries";
 
 import {Notebook} from "@/types/Notebook";
+import generateNotebookSummaryPrompt from "@/prompts/commands/generateNotebookSummaryPrompt";
 
 export const generateNotebookSummary = async (notebookId: Notebook["id"]): Promise<string | null> => {
     const userNotebookSummaries = await findUserNotebookSummariesByNotebook(notebookId);
+
+    const prompt = generateNotebookSummaryPrompt(userNotebookSummaries);
 
     const response = await openai.chat.completions.create({
         model: process.env.GPT_MODEL_ID as string,
         messages: [
             {
                 role: "system",
-                content: `
-                    Each student's performance in the class is summarized.
-
-                    Your goal is to provide a two sentence summary of the class's overall performance and understanding of the course material, which will inform the teacher of their students' strengths and knowledge gaps.
-
-                    The summaries are as follows:
-
-                    ${userNotebookSummaries.map((summary) => JSON.stringify(summary)).join("\n")}
-
-                    Respond in the second person as if you are talking to the teacher.
-                    
-                    Respond in JSON format with the following structure:
-
-                    {
-                        summary: <string>
-                    }
-                    
-                    The summary should use markdown to format the text, bolding the most important information with asterisks.
-                `,
+                content: prompt,
             }
         ],
         response_format: {
