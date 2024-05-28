fd3e40e8c249c8de51fb21f2aa1df41ca4ef9f33
json <jason@hedmans.org>
2024-05-29 00:53:29 +0200

    add posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,18 @@ import theme from "@/theme";
 
 import type { AppProps } from 'next/app'
 
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
+
+if (typeof window !== 'undefined') { // checks that we are client-side
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+        loaded: (posthog) => {
+            if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
+        },
+    })
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <SessionProvider
@@ -17,7 +29,9 @@ export default function App({ Component, pageProps }: AppProps) {
               theme={theme}
           >
               <Analytics />
-              <Component {...pageProps} />
+              <PostHogProvider client={posthog}>
+                <Component {...pageProps} />
+              </PostHogProvider>
           </ChakraProvider>
       </SessionProvider>
   )
